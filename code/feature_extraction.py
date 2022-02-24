18d7f155af88910ce82467ba5677f4236d1a1afc
Lahorka <lahorka.nikolovski@gmail.com>
2022-02-24 20:49:20 +0100

    Extract features for arguments only

--- code/feature_extraction.py
+++ code/feature_extraction.py
@@ -2,6 +2,32 @@
 import csv
 
 
+def read_sentences_from_connlu(path):
+    sentences = []
+    with open(path, encoding='utf-8') as infile:
+        sentence = []
+        for line in infile:
+            if line.startswith('#'):
+                continue
+            else:
+                row = line.split()
+                if not sentence:
+                    length = len(row)  # memorize the length of the first token
+                if not row:  # if the row is empty, indicating sentence boundary
+                    sentences.append(sentence)
+                    sentence = []
+                elif len(row) > 11:  # sentence has 1 or more predicates
+                    sentence.append(row)
+                else:  # sentence doesn't have predicates
+                    while len(row) < 12:
+                        row.append('_')
+                    while len(row) < length:  # there are some weird sentences starting with indexes containing .
+                        # this accounts for them
+                        row.append('_')
+                    sentence.append(row)
+    return sentences
+
+
 def read_sentences_from_tsv(path):
     sentences = []
     with open(path, encoding='utf-8') as infile:
@@ -13,8 +39,6 @@ def read_sentences_from_tsv(path):
             else:  # empty lines indicate sentence boundaries
                 sentences.append(sentence)
                 sentence = []  # prepare a container for the next sentence
-        # sentences.append(sentence)  # append the last sentence if the last line of the file is not empty > check
-        # # after Rod finishes!
     return sentences
 
 
@@ -26,9 +50,11 @@ def construct_a_sentence(sentence, pred_column, i):
 
 
 def extract_predicate_lemma(sentence):
+    # iterate through all the tokens in the sentence until you find the predicate, then return its lemma
     for token in sentence:
         if token[-1] == 'V':
             return token[2]
+    # if there are no predicates, returns None
 
 
 def extract_lemma(token):
@@ -40,13 +66,15 @@ def extract_features_and_labels(sentence, pred_column):
     # extract features that depend on the whole sentence and are the same for all tokens
     predicate_lemma = extract_predicate_lemma(sentence)  # extract lemma of the predicate as feature
     for token in sentence:
-        # extract features that only depend on this token
-        lemma = extract_lemma(token)   # extract lemma as feature
-        # collect all features
-        features = [lemma, predicate_lemma]
-        # extract the label
-        label = [token[pred_column]]
-        new_sentence.append(features + label)
+        # check if token is an argument > we only extract features for arguments
+        if token[-1] not in ['V', '_']:
+            # extract features that only depend on this token
+            lemma = extract_lemma(token)   # extract lemma as feature
+            # collect all features
+            features = [lemma, predicate_lemma]
+            # extract the label
+            label = [token[pred_column]]
+            new_sentence.append(features + label)
     return new_sentence
 
 
@@ -76,17 +104,22 @@ def write_sentences_to_tsv(sents, path):
             for s in sent:
                 for token in s:
                     csvwriter.writerow(token)
-                if len(sent) > 1 and sent.index(s) != len(sent) - 1:
-                    csvwriter.writerow(['X'])  # indicate repeats of the same sentence
-            csvwriter.writerow(['Y'])  # indicate sentence breaks
+            #     if len(sent) > 1 and sent.index(s) != len(sent) - 1:
+            #         csvwriter.writerow(['X'])  # indicate repeats of the same sentence
+            # csvwriter.writerow(['Y'])  # indicate sentence breaks
 
 
-def main(path):
-    sentences = read_sentences_from_tsv(path)
+def main(train_path, test_path):
+    train_sentences = read_sentences_from_connlu(train_path)
+    test_sentences = read_sentences_from_tsv(test_path)
     pred_column = 11
-    new_sentences = extract_features_and_labels_from_sentences(sentences, pred_column)
-    write_sentences_to_tsv(new_sentences, path.replace('.tsv', '-features.tsv'))
+    # extract features and gold labels from the training file
+    new_sentences = extract_features_and_labels_from_sentences(train_sentences, pred_column)
+    write_sentences_to_tsv(new_sentences, '../data/train-features.tsv')
+    # extract features from the test file
+    new_sentences = extract_features_and_labels_from_sentences(test_sentences, pred_column)
+    write_sentences_to_tsv(new_sentences, '../data/dev-features.tsv')
 
 
 # test
-main('../data/dev2.tsv')
+main('../data/en_ewt-up-train.conllu', '../data/en_ewt-up-dev-rule-identification.tsv')
